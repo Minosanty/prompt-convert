3f15def01bb602b82f3f43e974673f2d39cfbf85
minoS <minosantiago17@gmail.com>
2025-05-12 09:16:42 -0300

    funcion de conversion

--- introduccion.js
+++ introduccion.js
@@ -1,19 +1,12 @@
 let Divisa = 1300;
-let convercion = "";
+let conversion = "";
 
-while (convercion !== "2") {
-    convercion = prompt("1) Divisa (ARS -> USD)\n2) Salir");
+while (conversion !== "2") {
+    conversion = prompt("1) Divisa (ARS -> USD)\n2) Salir");
 
-    switch (convercion) {
+    switch (conversion) {
         case "1":
-            let montoarg = parseFloat(prompt("Ingrese su valor en pesos"));
-
-            if (montoarg > 0) {
-                let resultadoUSD = montoarg / Divisa;
-                alert(`$${montoarg} ARS equivale a $${resultadoUSD.toFixed(2)} USD.`);
-            } else {
-                alert("Por favor, ingresá un número válido mayor que cero.");
-            }
+           
             break;
         case "2":
             alert("Gracias por usar la app.");
